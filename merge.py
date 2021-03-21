523ba24191a11852048b80e7355d5e62d49f628e
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-03-22 01:18:13 +0900

    マージソート

---
+++ merge.py
@@ -0,0 +1,37 @@
+from typing import List
+
+def merge_sort(numbers: List[int]) -> List[int]:
+    if len(numbers) <= 1:
+        return numbers
+    
+    center = len(numbers) // 2
+    left = numbers[:center]
+    right = numbers[center:]
+    
+    merge_sort(left)
+    merge_sort(right)
+    
+    i = j = k = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            numbers[k] = left[i]
+            i += 1
+        else:
+            numbers[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        numbers[k] = left[i]
+        i += 1
+        k += 1
+        
+    while j< len(right):
+        numbers[k] = right[j]
+        j += 1
+        k += 1
+    return numbers
+print(merge_sort([
+    394, 808, 4820, 38, 409, 30, 342
+]))
+
