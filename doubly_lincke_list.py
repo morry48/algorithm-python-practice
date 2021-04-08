00db04a8ea8258c240c687fbbc6864afa12e6d55
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-04-09 00:47:38 +0900

    そーと

--- doubly_lincke_list.py
+++ doubly_lincke_list.py
@@ -99,13 +99,27 @@ class DoublyLinkedList(object):
             
             return _reverse_recursive(current_node.prev)
         self.head = _reverse_recursive(self.head)
-                
+        
+    
+    def sort(self) -> None:
+        if self.head is None:
+            return
+        
+        current_node = self.head
+        while current_node.next:
+            next_node = current_node.next
+            while next_node:
+                if current_node.data > next_node.data:
+                    current_node.data, next_node.data = next_node.data, current_node.data
+                next_node = next_node.next
+            current_node = current_node.next
+            
 if __name__ == '__main__':
     d = DoublyLinkedList()
-    d.append(0)    
+    d.append(4)    
     d.append(1)
-    d.append(2)
+    d.append(6)
     d.append(3)
     d.print()
-    d.reverse_recursive()
+    d.sort()
     d.print()
