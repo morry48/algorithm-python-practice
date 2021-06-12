02b6b38dbbd785ca0d8164a23b863333c0c744b3
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-06-12 18:52:36 +0900

    queue

--- queue.py
+++ queue.py
@@ -1,24 +1,17 @@
-from typing import Any
 from collections import deque
 
-class Queue(object):
-    def __init__(self) -> None:
-        self.queue = []
-        
-    def enqueue(self, data: Any) -> None:
-        self.queue.append(data)
-        
-    def dequeue(self) -> Any:
-        if self.queue:
-            return self.queue.pop(0)
-        
+def reverse(queue):
+    new_queue = deque()
+    while queue:
+        new_queue.append(queue.pop())
+    return new_queue        
+
 if __name__ == '__main__':
-    q = Queue()
-    q.enqueue(2)
-    q.enqueue(3)
-    q.enqueue(4)
-    q.dequeue()
+    q = deque()
+    q.append(1)
+    q.append(2)
+    q.append(3)
+    q.append(4)
     print(q)
-    
-    
-    
+    q = reverse(q)
+    print(q)
